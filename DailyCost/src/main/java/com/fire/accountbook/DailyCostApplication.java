96eaa210c8f92e4606e376a9fb4c2a2f93f674f9
fire <1747834798@qq.com>
2020-11-14 00:00:05 +0800

    first commit

---
+++ DailyCost/src/main/java/com/fire/accountbook/DailyCostApplication.java
@@ -0,0 +1,13 @@
+package com.fire.accountbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyCostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyCostApplication.class, args);
+	}
+
+}
---
+++ DailyCost/src/main/java/com/fire/accountbook/entity/User.java
@@ -0,0 +1,12 @@
+package com.fire.accountbook.entity;
+
+import javax.persistence.Entity;
+
+import lombok.Data;
+
+@Data
+@Entity(name = "t_user")
+public class User {
+	
+
+}
